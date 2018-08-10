22f2faf235c2c48bd044926c3a47da3b0377f872
Landver <good1ni@mail.ru>
2018-08-10 20:17:54 +0300

    page 32

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
-  templateUrl: './hello-world.component.html',
-  styleUrls: ['./hello-world.component.css']
+  template: `
+    <p>
+      hellow-world works inline!
+    <p>
+  `
 })
 export class HelloWorldComponent implements OnInit {
 
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html'
+})
+export class UserListComponent implements OnInit {
+  names: string[];
+
+  constructor() {
+    this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+  }
+
+  ngOnInit() {
+  }
+
+}
